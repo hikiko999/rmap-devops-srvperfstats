0a67cd25fb04baf354d39dd303fff7c1dfe77eab
Leif Christian Rosendo <lcrosendo@ngkhai.com>
2025-04-07 16:42:33 +0800

    Add memory total

--- server-stats.sh
+++ server-stats.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# CPU Total
 top -b -n 1 | grep '%Cpu(s)' | awk '{total=$2 + $4 + $6 + $ 10; print "CPU Total: " total "%"}'
+
+# Memory Total
+mem_top_line=$(top -b -n 1 | grep "MiB Mem")
+mem_total=$(echo "$mem_top_line" | awk '{print $4}')
+mem_free=$(echo "$mem_top_line" | awk '{print $6}')
+mem_used=$(echo "$mem_top_line" | awk '{print $8}')
+
+mem_used_percentage=$(echo "scale=2; ($mem_used / $mem_total) * 100" | bc)
+mem_free_percentage=$(echo "scale=2; ($mem_free / $mem_total) * 100" | bc)
+
+echo "Total Memory(MiB): $mem_total \
+| Free Memory(MiB): $mem_free($mem_free_percentage%) \
+| Used Memory(MiB): $mem_used($mem_used_percentage%)"
+# Consider buffer memory 5%
+
